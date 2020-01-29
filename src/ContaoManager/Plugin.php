20055e26e26e7f8e7b2e34d91c50effec36bab9c
Mario Gienapp <mario.gienapp@postyou.de>
2020-01-29 08:08:02 +0100

    conversion to bundle

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Postyou\ContaoRhymeSalesforceBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Postyou\ContaoRhymeSalesforceBundle\ContaoRhymeSalesforceBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(ContaoRhymeSalesforceBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class, 'phpforce/soap-client']),
+        ];
+    }
+}
---
+++ src/ContaoRhymeSalesforceBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Postyou\ContaoRhymeSalesforceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ContaoRhymeSalesforceBundle extends Bundle
+{
+}
